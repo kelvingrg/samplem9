0b0b7787b0615b0c0df6ece5ee587ead254771d1
Kelvin George <kelvinpktl@gmail.com>
2023-05-03 21:28:30 +0530

    frontend css bugs sorted

---
+++ samplejs.js
@@ -0,0 +1,5 @@
+var readlineSync = require("readline-sync");
+let no1 = readlineSync.question("number 1 :");
+let no2 = readlineSync.question("number 2:");
+let sum=parseInt(no1)+parseInt(no2)
+console.log("Hi " ,sum);
